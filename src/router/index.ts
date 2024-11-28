5cc732554e35cb3d61c5e8fe595f570617f4b929
Wildan <wildanalifrio@gmail.com>
2024-11-28 11:57:15 +0700

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
+      path: '/homepage',
+      name: 'homepage',
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
+})
+
+export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,3 @@
+<template>
+  <p class="text-3xl font-extrabold">ini template</p>
+</template>
